1969173bd9edfa05b3d729936a4491c7ff8206f7
deepak <deepakkushwahsk@gmail.com>
2024-05-28 16:46:54 +0530

    Fix rspec helper issue

--- app/controllers/str_calculate_controller.rb
+++ app/controllers/str_calculate_controller.rb
@@ -2,9 +2,17 @@ class StrCalculateController < ApplicationController
 	protect_from_forgery with: :null_session # This allows POST requests without CSRF tokens
 
 	def add
+		begin
 			numbers = params[:numbers]
-			return 0 if numbers.empty?
-			render json: { result: "this is for test" }
+			result = Calculate(numbers)
+			render json: { result: result }
+		rescue => e
+			render json: { error: e.message }, status: :bad_request
+		end
 	end
+
+	def Calculate(numbers)
+		return 0 if numbers.empty?
+end
 
 end
--- spec/requests/str_calculate_spec.rb
+++ spec/requests/str_calculate_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "StrCalculates", type: :request do
- describe "POST #add" do
+RSpec.describe "StrCalculateController", type: :request do
+  describe "POST #add" do
+    it "return 0 for empty string" do
+      post '/str_calculate/add', params: { numbers: "" }
+      expect(response.body).to eq("{\"result\":0}")
+    end
+
     
   end
 end
