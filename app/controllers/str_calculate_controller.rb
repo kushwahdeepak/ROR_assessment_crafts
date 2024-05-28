e94986b4ea3bbc9ac1a88475d4ef3fce0b27402b
deepak <deepakkushwahsk@gmail.com>
2024-05-28 17:05:28 +0530

    create logic for check negative numbers not allowed with rspec

--- app/controllers/str_calculate_controller.rb
+++ app/controllers/str_calculate_controller.rb
@@ -1,18 +1,27 @@
 class StrCalculateController < ApplicationController
 	protect_from_forgery with: :null_session # This allows POST requests without CSRF tokens
 
-	def add
-		begin
-			numbers = params[:numbers]
-			result = Calculate(numbers)
-			render json: { result: result }
-		rescue => e
-			render json: { error: e.message }, status: :bad_request
-		end
-	end
+    def add
+      begin
+        numbers = params[:numbers]
+        result = Calculate(numbers)
+        render json: { result: result }
+      rescue => e
+        render json: { error: e.message }, status: :bad_request
+      end
+    end
 
-	def Calculate(numbers)
-		return 0 if numbers.empty?
-end
+    def Calculate(numbers)
+        return 0 if numbers.empty?
+    
+        coma_seprate = ","
+        numbers = numbers.gsub("\n", coma_seprate)
+        nums = numbers.split(coma_seprate).map(&:to_i)
+    
+        negatives = nums.select { |n| n < 0 }
+        raise "negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+    
+        nums.sum
+    end
 
 end
--- spec/requests/str_calculate_spec.rb
+++ spec/requests/str_calculate_spec.rb
@@ -4,9 +4,20 @@ RSpec.describe "StrCalculateController", type: :request do
   describe "POST #add" do
     it "return 0 for empty string" do
       post '/str_calculate/add', params: { numbers: "" }
-      expect(response.body).to eq("{\"result\":0}")
+      expect(response.body).to eq("0")
     end
 
+    it "rais exception for negative numbers" do
+      post '/str_calculate/add', params: { numbers: "1,-2" }
+      expect(response.status).to eq(400)
+      expect(response.body).to eq("negative numbers not allowed: -2")
+    end
+   
+    it "rais exception for multiple negative numbers" do
+      post '/str_calculate/add', params: { numbers: "1,-2,-3" }
+      expect(response.status).to eq(400)
+      expect(response.body).to eq("negative numbers not allowed: -2,-3")
+    end
     
   end
 end
