a32ab63e8acb46a507d408eecf5700978d259041
deepak <deepakkushwahsk@gmail.com>
2024-05-28 15:51:13 +0530

    add spec file for str calculate controller

--- spec/requests/str_calculate_spec.rb
+++ spec/requests/str_calculate_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "StrCalculates", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+ describe "POST #add" do
+    
   end
 end
