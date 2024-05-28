be8716f0245dd55c12f3781ba5ca331964adced2
deepak <deepakkushwahsk@gmail.com>
2024-05-28 15:40:31 +0530

    create add method in controller & create routes for that

--- app/controllers/str_calculate_controller.rb
+++ app/controllers/str_calculate_controller.rb
@@ -1,2 +1,10 @@
 class StrCalculateController < ApplicationController
+	protect_from_forgery with: :null_session # This allows POST requests without CSRF tokens
+
+	def add
+			numbers = params[:numbers]
+			return 0 if numbers.empty?
+			render json: { result: "this is for test" }
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'str_calculate/add', to: 'str_calculate#add'
+
 end
